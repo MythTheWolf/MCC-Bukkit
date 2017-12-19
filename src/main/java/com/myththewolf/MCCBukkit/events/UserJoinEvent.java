ab866c45a7f85572199eacca5910ec2e8127dbd4
MythTheWolf <myth.wolf6@gmail.com>
2017-12-18 21:25:45 -0600

    Update the event to actually a user join event

--- src/main/java/com/myththewolf/MCCBukkit/events/UserJoinEvent.java
+++ src/main/java/com/myththewolf/MCCBukkit/events/UserJoinEvent.java
@@ -1,11 +1,24 @@
 package com.myththewolf.MCCBukkit.events;
 
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.json.JSONObject;
+import com.myththewolf.MCCBukkit.Main;
+import com.myththewolf.MCCBukkit.lib.SocketRequest;
+import com.myththewolf.MCCBukkit.lib.StatusType;
 
 public class UserJoinEvent implements Listener {
 
-  public void onChat(AsyncPlayerChatEvent event) {
-
+  public void onJoin(PlayerJoinEvent event) {
+    JSONObject packet = new JSONObject();
+    packet.put("packetType", "user-join");
+    packet.put("username", event.getPlayer().getName());
+    SocketRequest SR = new SocketRequest(Main.connectionSocket, packet);
+    SR.queue(result -> {
+      if (result.getStatus() != StatusType.SUCCESS) {
+        Main.getPlugin(Main.class).getLogger().severe("Packet delivery to server failed: "
+            + packet.toString() + "[Status: " + result.getRawResult() + "]");
+      }
+    });
   }
 }
